8c83dcba5ce52fcb6f281b6210bc37442bc77769
Rafael Reina <rafaelreina366@gmail.com>
2026-09-21 21:58:31 -0300

    Delete pokedex-backend directory

--- pokedex-backend/src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const config = require('./config');
-
-app.listen(config.port, () => {
-  console.log(`🚀 PokéDex API rodando em http://localhost:${config.port}`);
-  console.log(`   Consultando a PokéAPI em: ${config.pokeApiBaseUrl}`);
-});
